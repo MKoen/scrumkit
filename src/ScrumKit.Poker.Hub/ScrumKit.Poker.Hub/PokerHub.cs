41a04fb9a75c6bec19b8f4f2007fad125facdff1
Koen Missault <koen.missault@outlook.com>
2021-10-06 13:53:10 +0200

    Added poker SignalR hub

---
+++ src/ScrumKit.Poker.Hub/ScrumKit.Poker.Hub/PokerHub.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace ScrumKit.Poker.Hub
+{
+    public class PokerHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync().ConfigureAwait(false);
+
+            var caller = Clients.Caller;
+
+            // TODO
+        }
+    }
+}
